ecd5e485a139870351ad9f2ae96129410376c915
stevieboy1980 <syoung71580@gmail.com>
2024-10-15 22:32:54 -0700

    Week 11 assignment

--- app.js
+++ app.js
@@ -41,9 +41,10 @@ const winningOutcomes = [
   [box0, box4, box8],
   [box2, box4, box6],
 ];
-
+// End the game by disabling further clicks
 const endGame = () => {
   console.log("GAME OVER");
+  // Disables all boxes
   $(".box").css("pointer-events", "none");
   $('#p1').removeClass ("bg-dark border border-danger");
   $('#p2').removeClass ("bg-dark border border-warning");
@@ -57,13 +58,11 @@ const checkWinner = (currentPlayer, a, b, c) => {
     winner = true;
     console.log(`You are the winner ${currentPlayer}!`);
 
-    a.removeClass("text-dark bg-success");
-    b.removeClass("text-dark bg-success");
-    c.removeClass("text-dark bg-success");
+    a.removeClass("text-dark bg-success").addClass("text-success bg-light");
+    b.removeClass("text-dark bg-success").addClass("text-success bg-light");
+    c.removeClass("text-dark bg-success").addClass("text-success bg-light");
 
-    a.addClass("text-success bg-light");
-    b.addClass("text-success bg-light");
-    c.addClass("text-success bg-light");
+    
 
     if (currentPlayer === "X") {
       currentPlayer = "Player1";
@@ -77,17 +76,12 @@ const checkWinner = (currentPlayer, a, b, c) => {
     endGame();
   }
 };
-
+// Checks all possible winning outcomes
 const checkOutcomes = () => {
-  checkWinner(currentPlayer, ...winningOutcomes[0]);
-  checkWinner(currentPlayer, ...winningOutcomes[1]);
-  checkWinner(currentPlayer, ...winningOutcomes[2]);
-  checkWinner(currentPlayer, ...winningOutcomes[3]);
-  checkWinner(currentPlayer, ...winningOutcomes[4]);
-  checkWinner(currentPlayer, ...winningOutcomes[5]);
-  checkWinner(currentPlayer, ...winningOutcomes[6]);
-  checkWinner(currentPlayer, ...winningOutcomes[7]);
-
+    winningOutcomes.forEach(outcome => {
+      checkWinner(currentPlayer, ...outcome);
+    });
+// This checks for draw if no winning outcome
   if(turn === 9 && winner === false){
     endGame();
     $("#alertDraw").show();
@@ -95,46 +89,48 @@ const checkOutcomes = () => {
 };
 
 const startGame = () => {
-  console.log("Start Game!");
-  console.log(turn++);
-  currentPlayer = player1;
-  console.log(currentPlayer);
-
-  $("#p1").addClass("bg-dark border border-danger");
-
-  // show the start alert
-  $("#alertStart").show();
-
-  $(".box").on("click", function () {
-    $("#alertStart").hide();
-
-    $(this).text(currentPlayer);
-
-    if (turn > 4) {
-      //check winners
-      console.log("winner???");
-      checkOutcomes();
-    }
-
-    if (winner === false) {
-      if (currentPlayer === player1) {
-        currentPlayer = player2;
-        console.log(turn++);
-        $("#p2").addClass("bg-dark border border-warning");
-        $("#p1").removeClass("bg-dark border border-danger");
+    console.log("Start Game!");
+    turn = 0; // Reset turn at the start
+    currentPlayer = player1; // Set starting player
+    console.log(currentPlayer);
+  
+    $("#p1").addClass("bg-dark border border-danger");
+    $("#alertStart").show();
+  
+    // Disable the start button after the game starts
+    $("#startBtn").prop("disabled", true);
+  
+    $(".box").on("click", function () {
+      $("#alertStart").hide();
+  
+      // Check if the clicked box is empty and the game is still ongoing
+      if ($(this).text() === "" && !winner) {
+        $(this).text(currentPlayer);
+        turn++;
+  
+        if (turn > 4) {
+          console.log("Checking for a winner...");
+          checkOutcomes();
+        }
+        //ternary operator to toggle between players using jquery
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+        $("#p1").toggleClass("bg-dark border border-danger");
+        $("#p2").toggleClass("bg-dark border border-warning");
+      } else if (winner) {
+        console.log("Game is over!");
       } else {
-        currentPlayer = player1;
-        console.log(turn++);
-        $("#p1").addClass("bg-dark border border-danger");
-        $("#p2").removeClass("bg-dark border border-warning");
+        console.log("Box already occupied!");
       }
-    }
+    });
+  };
+  
+  // Reset button logic
+  document.getElementById("resetBtn").addEventListener("click", () => {
+    document.location.reload(true);
+    $("#startBtn").prop("disabled", false); // Re-enable the start button after reset
   });
-};
-
-document
-  .getElementById("startBtn")
-  .addEventListener("click", () => startGame());
-document
-  .getElementById("resetBtn")
-  .addEventListener("click", () => document.location.reload(true));
+  
+  // Event listener for start button
+  document.getElementById("startBtn").addEventListener("click", () => startGame());
+  
+  
